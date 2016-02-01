2b1cf62b8dc4abffac28ba8f83a45ad5309bd92f
anazalea <anneyagolob@gmail.com>
2016-01-31 21:32:10 -0400

    Fixed python path issues

---
+++ init_PySEDfitOG.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 31 21:17:53 2016
+
+@author: anneya
+                      .-. 
+                 .--.(   ).--.                
+      <-.  .-.-.(.->          )_  .--.
+       `-`(     )-'             `)    )
+         (o  o  )                `)`-'
+        (      )                ,)   
+        ( ()  )                 )
+         `---"\    ,    ,    ,/`  
+               `--' `--' `--'
+                |  |   |   | 
+                |  |   |   |
+                '  |   '   |  
+                
+"""
+
+from __future__ import print_function
+from __future__ import division
+
+import os
+import sys
+
+cwd = os.getcwd()
+sys.path.append(cwd+'/src/SEDManipulation/')
+sys.path.append(cwd+'/src/IO/')
+sys.path.append(cwd+'/src/ModelSelection/')
